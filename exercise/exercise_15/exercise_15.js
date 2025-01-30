03e416493287bb27f8ad6c007c0f17917d37acfa
fullstacksmax <fullstacksmax@gmail.com>
2025-01-29 21:28:54 -0600

    more down

--- exercise/exercise_15/exercise_15.js
+++ exercise/exercise_15/exercise_15.js
@@ -8,3 +8,30 @@ a string representing an operator ('+', '-', '*', '/', '%', '^') and returns
 the result of applying that operator to the two numbers. 
 
 Use a switch statement and ensure proper handling of invalid operators. */
+
+function evaluateOperation(a,b ,c) {
+    switch(b) {
+        case '+': 
+            return a + c;
+        case '-':
+            return a - c;
+        case '*':
+            return a * c;
+        case '/':
+            return a / c;
+        case '%':
+            return a % c;
+        case '^':
+            return a ** c;
+        default:
+            return "invalid operator";
+    }
+}
+
+console.log(evaluateOperation(3, "+", 4))
+console.log(evaluateOperation(3, "-", 4))
+console.log(evaluateOperation(3, "*", 4))
+console.log(evaluateOperation(12, "/", 4))
+console.log(evaluateOperation(30, "%", 4))
+console.log(evaluateOperation(3, "^", 4))
+console.log(evaluateOperation(3, "x", 4))
--- exercise/exercise_16/exercise_16.js
+++ exercise/exercise_16/exercise_16.js
@@ -16,3 +16,24 @@ or
 
 
 Use logical operators and parentheses to ensure the correct evaluation order.*/
+
+function validateInput(obj) {
+    if(obj.isGuest) {
+        return true;
+    }
+    if((obj.username != "" && obj.password != "") && obj.email.includes("@")){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+const user1 = {
+    isGuest: false,
+    username: "mvontz",
+    password: "abc123",
+    email: "max@max.com"
+}
+console.log(user1.email)
+console.log(validateInput(user1))
