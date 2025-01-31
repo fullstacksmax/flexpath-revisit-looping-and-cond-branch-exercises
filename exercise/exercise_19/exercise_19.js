f1b2c5c825645de45873c923994742cf0aa48291
fullstacksmax <fullstacksmax@gmail.com>
2025-01-31 07:30:30 -0600

    ex 19

--- exercise/exercise_19/exercise_19.js
+++ exercise/exercise_19/exercise_19.js
@@ -10,3 +10,23 @@ Write a function `normalizeInput` that accepts any value and returns:
 - hrows an error if the value is null or undefined.
 
 Use conditional logic and strict equality operators to implement this. */
+
+function normalizeInput(val) {
+    if(val === null || val === undefined) {
+        throw new Error("null or undefined")
+    }
+
+    if(val === 0 || val === "" || val === false || Number.isNaN(val)){
+        return "N/A"
+    }
+    else {
+        return val
+    }
+}
+
+console.log(normalizeInput(0))
+console.log(normalizeInput(false))
+console.log(normalizeInput(NaN))
+//console.log(normalizeInput(null))
+//console.log(normalizeInput(undefined))
+console.log(normalizeInput(2))
