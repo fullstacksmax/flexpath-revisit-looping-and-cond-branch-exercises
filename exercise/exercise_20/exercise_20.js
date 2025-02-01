88b759d764b687a19bb06b3def6d94eefa7f75e2
fullstacksmax <fullstacksmax@gmail.com>
2025-01-31 19:54:49 -0600

    ex 20 done, need to complete a couple

--- exercise/exercise_20/exercise_20.js
+++ exercise/exercise_20/exercise_20.js
@@ -10,3 +10,53 @@ Implement 'increment' and 'decrement' methods using prefix and postfix forms of
 the increment/decrement operators. 
 
 Ensure that the counter behaves correctly at the boundaries. */
+
+const CircularCounter = class {
+    max = 3;
+    count = 0;
+    increment = function () {
+        //console.log('function block')
+        if(this.count === this.max) {
+            this.count = 0
+            return this.count;
+        }
+        else {
+            //console.log('else block')
+            this.count++;
+            return this.count;
+        }
+    }
+
+    decrement = function () {
+        if(this.count === 0) {
+            this.count = this.max;
+            return this.count 
+        }
+        else {
+            this.count--;
+            return this.count
+        }
+    }
+    
+}
+
+const myclass = new CircularCounter
+
+//console.log(myclass)
+//console.log(myclass.max)
+//console.log(myclass.count)
+myclass.count = 0
+console.log(myclass.increment())
+console.log(myclass.increment())
+console.log(myclass.increment())
+console.log(myclass.increment())
+console.log(myclass.increment())
+console.log(myclass.decrement())
+console.log(myclass.decrement())
+console.log(myclass.decrement())
+console.log(myclass.decrement())
+console.log(myclass.decrement())
+console.log(myclass.increment())
+console.log(myclass.increment())
+console.log(myclass.increment())
+
